0b554390f5406ade8cb2e4cf171fcea6be962dd1
Felipe Foliatti <felipe.foliatti@soha.com.br>
2017-12-31 01:58:07 -0200

    logger.js public attribute adjustment

--- logger.js
+++ logger.js
@@ -3,19 +3,18 @@
 const mysql = require('mysql');
 
 class Logger {
-
-    static Tipo = Object.freeze({"WARN": "WARN", "ERROR":"ERROR", "INFO":"INFO", "FATAL":"FATAL" })
     
     constructor(){
     }
 
     log(level, message){
-        if(level == Tipo.ERROR || level == Tipo.FATAL){
-            console.error({message: message, timestamp: new Date().toISOString(), level: level}); 
+        if(level == Logger.Tipo.ERROR || level == Logger.Tipo.FATAL){
+            console.error({level: level, timestamp: new Date().toISOString(), message: message}); 
         } else {
-            console.info({message: message, timestamp: new Date().toISOString(), level: level}); 
+            console.info({level: level, timestamp: new Date().toISOString(), message: message}); 
         }
     }
 }
 
+Logger.Tipo = Object.freeze({"WARN": "WARN", "ERROR":"ERROR", "INFO":"INFO", "FATAL":"FATAL" });    
 module.exports.Logger = Logger;
