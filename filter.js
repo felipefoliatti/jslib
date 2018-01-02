2ffee3250cf408898a5bdecedfae323a931d9c8e
Felipe Foliatti <felipe.foliatti@soha.com.br>
2018-01-01 23:37:44 -0200

    filter added

---
+++ filter.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class Filter {
+    
+    constructor(field, data){
+        this.field =field;
+        this.data = data;
+        
+        if(!data){
+            this.field = "null"
+            this.operator = "is"
+            this.value = null;
+        }
+        else{
+            let regex = new RegExp(/^([><])?(.+)$/);
+            let valid = regex.test(data);
+
+            if(!valid) throw Error("invalid filter") 
+
+            let match = regex.exec(data);
+
+            this.operator = match[1] || "=";
+            this.value = match[2];
+        }
+    }
+    
+}
+
+module.exports.Filter = Filter
