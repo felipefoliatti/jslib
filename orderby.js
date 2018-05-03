8034ed0bb81b65cb1cfa0077a21895fe5309e015
Felipe Foliatti <felipe.foliatti@soha.com.br>
2018-05-03 13:02:35 -0300

    add allowed fields to orderby

--- orderby.js
+++ orderby.js
@@ -4,16 +4,21 @@ class Orderby {
 
     //config
     //  single: true/false
+    //  allowed: [] fields allowed 
     constructor(config){
         this.config = config;
     }
     
     parse (param) {
-        let valid = this.config.single? new RegExp(/^(([-+]\w+)|(\w+))*$/).test(param):new RegExp(/^(([-+]\w+)|(\w+))(,(([-+]\w+)|(\w+)))*$/).test(param);
-        if(!valid) throw Error("invalid order") 
+        var valid = this.config.single? new RegExp(/^(([-+]\w+)|(\w+))*$/).test(param):new RegExp(/^(([-+]\w+)|(\w+))(,(([-+]\w+)|(\w+)))*$/).test(param);
+        var unallowed = [];
+        var me = this;
 
-        let orders = param.split(',').map((field) =>{
-            let order = "";
+        if(!valid) throw Error("invalid order");
+
+
+        var orders = param.split(',').map((field) =>{
+            var order = "";
             if(field && field[0] == '-'){
                 order += " desc";
                 field = field.substring(1);
@@ -22,12 +27,23 @@ class Orderby {
                 order += " asc";
                 field = field.substring(1);
             } 
-              
+            
+            //If exist some allowed field, consider it
+            if (me.config.allowed && me.config.allowed.indexOf(field) > -1){
+                unallowed.push(field);
+            }
+            
             if(field){
                 order = field + order;
             }
             return order;
         }).join(",");
+
+        //If there is some unallowed field
+        if(unallowed){
+            throw Error("invalid field: " + unallowed.join(",")); 
+        }
+
         return orders; 
     }
 
