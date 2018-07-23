e3ea8f31e506b7e15129ac42f7459668e86ee079
Felipe Foliatti <felipe.foliatti@gmail.com>
2018-07-23 18:15:46 -0300

    added the option to have a function to act over the value

--- filter.js
+++ filter.js
@@ -1,11 +1,41 @@
 'use strict';
 
+/**
+ * Filter class
+ * The example above demonstrates how to use the filter.
+ * 
+ * @example
+ * var status = new filter.Filter('status', req.query.username, [{in: "", out: "="}])
+ * @description
+ * The variable status now has trhe properties: field, operator and value. Theese propertis can now be used to mount any query statement 
+ * 
+ * @class Filter
+ */
 class Filter {
     
+    /**
+     * @description
+     * Creates a new instance of a {@link Filter}. It has no methods, but has the fields above:
+     * 
+     * @property {String}  this.field the fieldname.
+     * @property {String}  this.operator  the operator.
+     * @property {Object}  this.value the filter value.
+     * 
+     * @description
+     * To generate the constructor, use the following parameters.
+     * 
+     * @param {String} field  the fieldname to be stored at {@link Filter#field}.
+     * @param {String} data the value of the filter to be evaluated in operations passed by the param op.
+     * @param {Object[]} op the array of operations to eval against the param data.
+     * @param {String} op[].in - the string searched in the parameter.
+     * @param {String} op[].out the string returned in the {@link Filter#operator}.
+     * @param {Function} op[].fn the action used parse the value in {@link Filter#value}. It can be null
+     * 
+     */
     constructor(field, data, op){
         this.field =field;
         this.data = data;
-        this.op = op || [{in: "=", out: "="}, {in: "", out: "="}, {in: ">", out: ">"}, {in: "<", out: "<"}, {in: "!", out: "<>"}];
+        this.op = op || [{in: "=", out: "=", fn: null}, {in: "", out: "=", fn: null}, {in: ">", out: ">", fn: null}, {in: "<", out: "<", fn: null}, {in: "!", out: "<>", fn: null}];
         
         if(!data){
             this.empty = true;
@@ -27,7 +57,7 @@ class Filter {
 
             this.empty = false;
             this.operator = operators[0].out;
-            this.value = match[2];
+            this.value = this.operator[0].fn? this.operator[0].fn(match[2]) : match[2];
         }
     }
     
