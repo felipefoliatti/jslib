d387c6ae8cfbf6c892e3e48c196fb2f6a3048182
Felipe Foliatti <felipe.foliatti@soha.com.br>
2023-08-03 12:02:32 -0300

    added date corrections

--- filter2.js
+++ filter2.js
@@ -15,39 +15,43 @@ class Filter {
 
     static OP = {
       STRING: {
-        EQ:  (opt) => Filter.assign({in: "", out: "=", fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },               opt, {custom: false}),
-        IN:  (opt) => Filter.assign({in: "", out: "in", fn: (e)=> e.replace(/\s/g,'').split(','), placeholder: '(?)' }, opt, {custom: false}),
-        LK:  (opt) => Filter.assign({in: "", out: "like", fn: (e)=> e.enclose("%"), placeholder: '?' },                 opt, {custom: false}),        
-        NEQ: (opt) => Filter.assign({in: "", out: "<>", fn: (e)=> e.replace(/\s/g,''), placeholder: '?'},               opt, {custom: false}),
+        EQ:  (opt) => Filter.assign({in: "", out: "=",   fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),             placeholder: '?' },   opt, {custom: false}),
+        IN:  (opt) => Filter.assign({in: "", out: "in",  fn: (e)=> ({data: e.replace(/\s/g,'').split(','), expand: false}),  placeholder: '(?)' }, opt, {custom: false}),
+        LK:  (opt) => Filter.assign({in: "", out: "like",fn: (e)=> ({data: e.enclose("%"), expand: false}),                  placeholder: '?' },   opt, {custom: false}),        
+        NEQ: (opt) => Filter.assign({in: "", out: "<>",  fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),             placeholder: '?'},    opt, {custom: false}),
       },
       BOOL: {
-        IN:  (opt) => Filter.assign({in: "", out: "in", fn: (e)=> e.replace(/\s/g,'').split(','), placeholder: '(?)' }, opt, {custom: false}),
-        EQ:  (opt) => Filter.assign({in: "", out: "=",  fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },              opt, {custom: false}),        
-        NEQ: (opt) => Filter.assign({in: "", out: "<>", fn: (e)=> e.replace(/\s/g,''), placeholder: '?'},               opt, {custom: false}),
+        IN:  (opt) => Filter.assign({in: "", out: "in", fn: (e)=> ({data: e.replace(/\s/g,'').split(','), expand: false}), placeholder: '(?)' }, opt, {custom: false}),
+        EQ:  (opt) => Filter.assign({in: "", out: "=",  fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?' },   opt, {custom: false}),        
+        NEQ: (opt) => Filter.assign({in: "", out: "<>", fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?'},    opt, {custom: false}),
       },
       INT: {
-        EQ:  (opt) => Filter.assign({in: "", out: "=",  fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },               opt, {custom: false}),
-        IN:  (opt) => Filter.assign({in: "", out: "in", fn: (e)=> e.replace(/\s/g,'').split(','), placeholder: '(?)' },  opt, {custom: false}),
-        GT:  (opt) => Filter.assign({in: "", out: ">",  fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },               opt, {custom: false}),
-        GTE: (opt) => Filter.assign({in: "", out: ">=", fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },               opt, {custom: false}),
-        LT:  (opt) => Filter.assign({in: "", out: "<",  fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },               opt, {custom: false}),
-        LTE: (opt) => Filter.assign({in: "", out: "<=", fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },               opt, {custom: false}),
-        NEQ: (opt) => Filter.assign({in: "", out: "<>", fn: null, placeholder: '?'}, opt),
+        EQ:  (opt) => Filter.assign({in: "", out: "=",  fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?'   }, opt, {custom: false}),
+        IN:  (opt) => Filter.assign({in: "", out: "in", fn: (e)=> ({data: e.replace(/\s/g,'').split(','), expand: false}), placeholder: '(?)' }, opt, {custom: false}),
+        GT:  (opt) => Filter.assign({in: "", out: ">",  fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?'   }, opt, {custom: false}),
+        GTE: (opt) => Filter.assign({in: "", out: ">=", fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?'   }, opt, {custom: false}),
+        LT:  (opt) => Filter.assign({in: "", out: "<",  fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?'   }, opt, {custom: false}),
+        LTE: (opt) => Filter.assign({in: "", out: "<=", fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?'   }, opt, {custom: false}),
+        NEQ: (opt) => Filter.assign({in: "", out: "<>", fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}),            placeholder: '?'   }, opt),
       },
       DATE: {
-        EQ:  (opt) => Filter.assign({in: "", out: "=",  fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },                                                                                            opt, {custom: false}),
-        GT:  (opt) => Filter.assign({in: "", out: ">",  fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },                                                                                            opt, {custom: false}),
-        GTE: (opt) => Filter.assign({in: "", out: ">=", fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },                                                                                           opt, {custom: false}),
-        LT:  (opt) => Filter.assign({in: "", out: "<",  fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },                                                                                            opt, {custom: false}),
-        LTE: (opt) => Filter.assign({in: "", out: "<=", fn: (e)=> e.replace(/\s/g,''), placeholder: '?' },                                                                                           opt, {custom: false}),
-        BTW: (opt) => Filter.assign({in: "", lop: ">=", rop: "<=", query: (value, operator)=> `:field ${operator.lop} ? AND :field ${operator.rop} ?`, fn: (e)=> e.replace(/\s/g,'') },              opt, {custom: true}),
-
+        EQ:  (opt) => Filter.assign({in: "", out: "=",                                                                                                 fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}), placeholder: '?' },           opt, {custom: false}),
+        GT:  (opt) => Filter.assign({in: "", out: ">",                                                                                                 fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}), placeholder: '?' },           opt, {custom: false}),
+        GTE: (opt) => Filter.assign({in: "", out: ">=",                                                                                                fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}), placeholder: '?' },           opt, {custom: false}),
+        LT:  (opt) => Filter.assign({in: "", out: "<",                                                                                                 fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}), placeholder: '?' },           opt, {custom: false}),
+        LTE: (opt) => Filter.assign({in: "", out: "<=",                                                                                                fn: (e)=> ({data: e.replace(/\s/g,''), expand: false}), placeholder: '?' },           opt, {custom: false}),
+        BTW: (opt) => Filter.assign({in: "", lop: ">=", rop: "<=", query: (value, operator)=> `:field ${operator.lop} ? AND :field ${operator.rop} ?`, fn: (e)=> ({data: Filter.require(e.replace(/\s/g,'').split(','),2), expand: true}) }, opt, {custom: true}),
       },
-      CUSTOM: (opt) => Filter.assign({in: "", query: ()=>"", fn: (e)=> e.replace(/\s/g,'') }, opt, {custom: true})
+      CUSTOM: (opt) => Filter.assign({in: "", query: ()=>"", fn: (e)=>  ({data: e.replace(/\s/g,''), expand: false}) }, opt, {custom: true})
     }
   
     static assign(...objects){
       return objects.reduce((p, c) => Object.assign(p, c), {})
+    }
+
+    static require(array, count){
+      if (!array || array.length != count){ throw new Error('invalid filter, requiring two arguments: ' + array) }
+      return array;
     }
   
     constructor(field, value, ...op){
@@ -57,12 +61,12 @@ class Filter {
   
       this.operation = '';
       this.value = [];
-      
-  
+
+      //make the value always an array
       if (!Array.isArray(value)){
-        value = [value];
+        value = [value]
       }
-  
+       
       let contexts = [];
       for(let i=0; i < value.length; i ++){
         
@@ -73,33 +77,30 @@ class Filter {
             context.field = "null"
             context.operator = "is"
             context.operation = `(${context.field} ${context.operator} NULL)`;
-            context.value = [];
+            context.value = [{data: undefined, expand: false}];
         }else{
-            let or = this.op.map(m => { return m.in }).join("|");
-            let regex = new RegExp(`^([${or}])?(.+)$`);
-            let valid = regex.test(context.value);
-  
-            if(!valid) throw Error("invalid filter");
-  
-            let match = regex.exec(context.value);
-            let operators = this.op.filter(e => context.value.startsWith(e.in) || e.in == "").reverse(); //the first find is the last item
+           
+            let operators = this.op.filter(e => context.value.startsWith(e.in) || e.in == "").reverse(); //the first find is the last item            
+
+            if (!operators.length) throw Error(`operator in '${context.value}' not found`);
+            let operator = operators[0];    
             
-            if (!operators.length) throw Error(`operator '${match[1]}' not found`);
-            let operator = operators[0];
-  
+            //clear the data removing the prefix
+            context.value = context.value.slice(operator.in.length);
+
             if (!operator.custom){
-              let value = operator.fn? operator.fn(match[2]) : match[2];
+              let value = operator.fn? operator.fn(context.value) : {data: context.value, expand: false};
               context.empty = false;
               context.operator = operator.out;
               context.value = [value];
               context.operation = `(${context.field} ${operator.out} ${operator.placeholder})`;
             } else {
-              let value = operator.fn? operator.fn(match[2]) : match[2]; 
-              let query = operator.query(value, operator)
-  
+              let value = operator.fn? operator.fn(context.value) : {data: context.value, expand: false}; 
+              let query = operator.query(value.data, operator)
+              
               context.empty = false;
               context.operator = null;
-              context.value = (query.match(/\?/g)||[]).map(() => value); //repeat the value as many as we have ?
+              context.value = value.expand? [value] : (query.match(/\?/g)||[]).map(() => value); //if we don't need to expand it, we have to fill the ? with data
               context.operation = `(${query.replace(/\:field/g, this.field)})`;
             }
         }
@@ -108,7 +109,23 @@ class Filter {
       }
   
       this.operation = `(${contexts.map(e => e.operation).join(' AND ')})`;
-      this.value = contexts.map(e => e.value).flat();
+      this.value = []
+      
+      contexts.forEach(context => {
+        //iterate over value: it is always an array
+        context.value.forEach(value => {
+          if (Array.isArray(value.data)){
+            if (value.expand){
+              this.value.push(...value.data)
+            }else {
+              this.value.push(value.data)
+            }
+          } else if (value.data !== undefined) {
+            //undefined should not be add
+            this.value.push(value.data)
+          } 
+        });    
+      });      
       
   }
 }  
