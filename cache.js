a068dc76b9b4549fdf20cfe279b47b7c55c0ab94
Felipe Foliatti <felipe.foliatti@soha.com.br>
2023-11-30 20:42:51 -0300

    added cache js

---
+++ cache.js
@@ -0,0 +1,113 @@
+'use strict';
+const request = require('request-promise-native');
+
+/**
+ * 
+ * 
+ */
+
+class Cache {
+
+    topic = null;
+    cache = null;
+    key = null;
+    fn = null;
+    handler = null;
+
+    static fn = {
+        DEFAULT_REQUEST: ({uri, version, limit}={}) => (async ({company, keys}={}) => await request({method: 'GET', uri: uri, qs: { version: (version || '1'), company: company, id: keys.join(','), limit: (limit || '*') },json: true})),
+        DEFAULT_RESPONSE_HANDLER: { getData: (response) => response.data, getKey: (item) => item.id },
+        DEFAULT_TOPIC_ID_HANDLER: (object) => object.id,
+    };
+  
+    // static assign(...objects){
+    //   return objects.reduce((p, c) => Object.assign(p, c), {})
+    // }
+
+    // static require(array, count){
+    //   if (!array || array.length != count){ throw new Error('invalid filter, requiring two arguments: ' + array) }
+    //   return array;
+    // }
+
+    static unique(array){
+        let distinct = new Set(array);
+        return Array.from(distinct) 
+    }
+  
+    constructor({cache, topic, key, fn, handler}={}){
+        this.cache = cache;
+        this.topic = topic;
+        this.key = key;
+        this.fn = fn;
+        this.handler = handler;
+
+        //subscribe to topic 
+        this.topic.subscribe((data) =>{
+            let objects = Array.isArray(data)? data : [data];
+            
+            //iterate over messages 
+            for(let i in objects){
+                var object = JSON.parse(objects[i]);
+                if (object.type == 1 || object.type == 2){
+                    
+                    let id = this.handler.topic(object);
+                    let entry = `${key}:${id}`;
+
+                    console.log(`${new Date().toISOString()} - cache deleted ${entry}`);
+                    this.cache.del(entry);
+                }
+            }
+        });
+    }   
+
+    async get({company, keys}={}){
+        
+        keys = Cache.unique(keys);
+
+        let missing = [];
+        let cached = [];
+
+        //try to get from cache
+        for(let i=0; i< keys.length; i++){
+            let id = keys[i];
+            let item = this.cache.get(`${this.key}:${id}`);
+            
+            if (item){
+                cached.push(item);
+            } else {
+                missing.push(id);
+            }
+
+        }   
+
+        if (missing.length){
+            try{
+                //request missing items
+                let response = await this.fn({company: company, keys: missing});
+            
+                //process the response
+                let data = this.handler.response.getData(response)
+            
+                //iterate over item and cache it
+                for (let i=0; i < data.length; i++){
+                    let object = data[i];
+                    let key = this.handler.response.getKey(object);
+
+                    //add to cache and cached
+                    this.cache.set(`${this.key}:${key}`, object);
+                    cached.push(object);
+                }
+            } 
+            catch(ex){
+                console.error(ex);
+                throw ex;
+            }
+
+        }
+
+        //return all cached data
+        return cached;
+    }
+}  
+
+module.exports = Cache
